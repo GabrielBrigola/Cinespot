1807b0deab719008a1695531d9f92fd4d93ef60d
GabrielBrigola <163156683+GabrielBrigola@users.noreply.github.com>
2024-04-01 18:54:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,260 @@
+function star1click() {
+    var star1 = document.getElementById("s1");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+
+function star2click() {
+    var star1 = document.getElementById("s2");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+
+function star3click() {
+    var star1 = document.getElementById("s3");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+
+function star4click() {
+    var star1 = document.getElementById("s4");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+
+function star5click() {
+    var star1 = document.getElementById("s5");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+
+
+function star1Click() {
+    var star1 = document.getElementById("ss1");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+
+function star2Click() {
+    var star1 = document.getElementById("ss2");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+
+function star3Click() {
+    var star1 = document.getElementById("ss3");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+
+function star4Click() {
+    var star1 = document.getElementById("ss4");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+
+function star5Click() {
+    var star1 = document.getElementById("ss5");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function Star1Click() {
+    var star1 = document.getElementById("sss1");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function Star2Click() {
+    var star1 = document.getElementById("sss2");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function Star3Click() {
+    var star1 = document.getElementById("sss3");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function Star4Click() {
+    var star1 = document.getElementById("sss4");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function Star5Click() {
+    var star1 = document.getElementById("sss5");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function Star1ClicK() {
+    var star1 = document.getElementById("ssss1");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function Star2ClicK() {
+    var star1 = document.getElementById("ssss2");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function Star3ClicK() {
+    var star1 = document.getElementById("ssss3");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function Star4ClicK() {
+    var star1 = document.getElementById("ssss4");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function Star5ClicK() {
+    var star1 = document.getElementById("ssss5");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function STar1Click() {
+    var star1 = document.getElementById("sssss1");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function STar2Click() {
+    var star1 = document.getElementById("sssss2");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function STar3Click() {
+    var star1 = document.getElementById("sssss3");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function STar4Click() {
+    var star1 = document.getElementById("sssss4");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
+function STar5Click() {
+    var star1 = document.getElementById("sssss5");
+    var computedStyle = window.getComputedStyle(star1);
+
+    if (computedStyle.color === "rgb(138, 138, 138)") {
+        star1.style.color = "rgb(255, 217, 0)";
+    } else {
+        star1.style.color = "rgb(138, 138, 138)";
+    }
+}
